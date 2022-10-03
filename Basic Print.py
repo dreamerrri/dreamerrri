3f13ab4051bbf4c1ba87b926d4a27780bdcea75c
dreamerrri <114892885+dreamerrri@users.noreply.github.com>
2022-10-03 11:27:24 +0800

    Create Basic Print.py

---
+++ Basic Print.py
@@ -0,0 +1,7 @@
+#Write a python Program that will display your name, Course, and school.
+
+list = ["Renz Andrew S. Ebreo", "Bachelor of Science in Information Technology", "PHINMA AU"]
+
+print = ("Name: ", list[0])
+print = ("Course: ", list[1])
+print = ("School: ", list[2])
